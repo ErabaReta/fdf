6c6d248ce1bdf4c1b7a6c6bb6ae3674b16f4ac4b
ErabaReta <mokhtarouhrich@gmail.com>
2024-04-07 01:27:24 +0000

    adding ft_math.c file and code some math functions

--- fdf.h
+++ fdf.h
@@ -7,26 +7,44 @@
 #include "get_next_line/get_next_line.h"
 
 //===========================
-typedef struct	s_data {
+typedef struct	s_img_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}				t_data;
+}				t_img_data;
 //===========================
-typedef struct s_point
+typedef struct s_3d_point
 {
 	int	x;
 	int	y;
 	int	z;
-}	t_point;
+}				t_3d_point;
+//===========================
+typedef struct	s_2d_point
+{
+	int	x;
+	int	y;
+}				t_2d_point;
+//===========================
+typedef struct	s_matrix {
+	int		**value;
+	int		rows;
+	int		cols;
+}				t_matrix;
 //===========================
 typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-	t_data	*data;
+	void		*mlx;
+	void		*win;
+	t_matrix	map;
+	t_img_data	data;
 }				t_vars;
+//===========================
+typedef struct s_camera	{
+	t_3d_point pov;
+	t_3d_point plane;
+}
 //===========================
 char	**ft_split(char const *s, char c);
 size_t	ft_strlen(const char *s);
