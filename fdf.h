ca1c8466cafc446f9dcabfc028e7da355f34442a
El Mokhtar Ouhrich <eouhrich@e2r12p2.1337.ma>
2024-04-13 02:51:28 +0000

    turn map of ints into a double array of matrix called shape

--- fdf.h
+++ fdf.h
@@ -4,6 +4,7 @@
 #include <mlx.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include<math.h>
 #include "get_next_line/get_next_line.h"
 
 //===========================
@@ -17,9 +18,9 @@ typedef struct	s_img_data {
 //===========================
 typedef struct s_3d_point
 {
-	int	x;
-	int	y;
-	int	z;
+	float	x;
+	float	y;
+	float	z;
 }				t_3d_point;
 //===========================
 typedef struct	s_2d_point
@@ -29,27 +30,35 @@ typedef struct	s_2d_point
 }				t_2d_point;
 //===========================
 typedef struct	s_matrix {
-	int		**value;
+	float		**value;
 	int		rows;
 	int		cols;
 }				t_matrix;
 //===========================
 typedef struct	s_vars {
+	int			height;
+	int			width;
 	void		*mlx;
 	void		*win;
-	t_matrix	map;
 	t_img_data	data;
+	t_matrix	**shape;
 }				t_vars;
 //===========================
 typedef struct s_camera	{
 	t_3d_point pov;
 	t_3d_point plane;
-}
+}				t_camera;
 //===========================
 char	**ft_split(char const *s, char c);
 size_t	ft_strlen(const char *s);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*free_all(char **ptr, int i);
 int	ft_atoi(const char *str);
+
+//=
+int	ft_power(int nbr, int power);
+int ft_abs(int nbr);
+int calc_dist(int x, int y, t_2d_point point);
+char	matrix_is_valid(t_matrix matrix);
 
 #endif
--- ft_math.c
+++ ft_math.c
@@ -24,73 +24,73 @@ int calc_dist(int x, int y, t_2d_point point)
 	r = sqrt(ft_power(ft_abs(x - point.x), 2) + ft_power(ft_abs(y - point.y), 2));
 	return (r);
 }
-char	matrix_is_valid(t_matrix matrix)
-{
-	int	i;
-	int j;
+// char	matrix_is_valid(t_matrix matrix)
+// {
+// 	int	i;
+// 	int j;
 
-	if (matrix.value == 0 || matrix.rows <= 0 || matrix.cols <= 0)
-		return (0);
-	i = 0;
-	while (i < matrix.rows)
-	{
-		j = 0;
-		while (j < matrix.cols)
-		{
-			if (matrix.value[i][j] == NULL)
-				return (0);
-		}
-	}
-	return (1);
-}
+// 	if (matrix.value == 0 || matrix.rows <= 0 || matrix.cols <= 0)
+// 		return (0);
+// 	i = 0;
+// 	while (i < matrix.rows)
+// 	{
+// 		j = 0;
+// 		while (j < matrix.cols)
+// 		{
+// 			if (matrix.value[i][j] == NULL)
+// 				return (0);
+// 		}
+// 	}
+// 	return (1);
+// }
 
-long	matrix_dot_product(t_matrix matrix1, int row, t_matrix matrix2, int col)
-{
-	long	result;
-	int		i;
+// long	matrix_dot_product(t_matrix matrix1, int row, t_matrix matrix2, int col)
+// {
+// 	long	result;
+// 	int		i;
 
-	i = 0;
-	result = 0;
-	while (i < matrix1.cols && i < matrix2.rows)
-	{
-		result += matrix1.value[row][i] * matrix2.value[i][col];
-		i++;
-	}
-	return (result);
-}
+// 	i = 0;
+// 	result = 0;
+// 	while (i < matrix1.cols && i < matrix2.rows)
+// 	{
+// 		result += matrix1.value[row][i] * matrix2.value[i][col];
+// 		i++;
+// 	}
+// 	return (result);
+// }
 
-t_matrix	matrix_mult(t_matrix matrix1, t_matrix matrix2)
-{
-	int			i;
-	int			j;
-	t_matrix	result;
+// t_matrix	matrix_mult(t_matrix matrix1, t_matrix matrix2)
+// {
+// 	int			i;
+// 	int			j;
+// 	t_matrix	result;
 
-	if (matrix_is_valid(matrix1) == 0 || matrix_is_valid(matrix2) == 0)
-	{
-		result.value = 0;
-		return (result);
-	}
+// 	if (matrix_is_valid(matrix1) == 0 || matrix_is_valid(matrix2) == 0)
+// 	{
+// 		result.value = 0;
+// 		return (result);
+// 	}
 
-	result.rows = matrix1.rows;
-	result.cols = matrix2.cols;
-	result.value = (int **)malloc(sizeof(int *) * result.rows);
-	i = 0;
-	while (i < result.rows)
-	{
-		result.value[i] = (int *)malloc(sizeof(int) * result.cols);
-		i++;
-	}
-	i = 0;
-	j = 0;
-	while (i < result.rows)
-	{
-		j = 0;
-		while (j < result.cols)
-		{
-			result.value[i][j] = matrix_dot_product(matrix1, i, matrix2, j);
-			j++;
-		}
-		i++;
-	}
-	return (result);
-}
+// 	result.rows = matrix1.rows;
+// 	result.cols = matrix2.cols;
+// 	result.value = (int **)malloc(sizeof(int *) * result.rows);
+// 	i = 0;
+// 	while (i < result.rows)
+// 	{
+// 		result.value[i] = (int *)malloc(sizeof(int) * result.cols);
+// 		i++;
+// 	}
+// 	i = 0;
+// 	j = 0;
+// 	while (i < result.rows)
+// 	{
+// 		j = 0;
+// 		while (j < result.cols)
+// 		{
+// 			result.value[i][j] = matrix_dot_product(matrix1, i, matrix2, j);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// 	return (result);
+// }
